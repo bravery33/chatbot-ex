0ad169c64789b536580c8e7dea9b78230fd93c6e
bravery33 <diddung83@gmail.com>
2025-06-10 11:23:33 +0900

    refactor: Extract backend logic into a separate llm.py module

--- chat.py
+++ chat.py
@@ -1,14 +1,7 @@
 # https://docs.streamlit.io/
 import streamlit as st
-import os
+from llm import get_ai_message
 
-from dotenv import load_dotenv
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
-from pinecone import Pinecone
 
 st.set_page_config(
     page_title='전세사기피해 상담 챗봇',
@@ -24,46 +17,6 @@ if 'message_list' not in st.session_state:
 for message in st.session_state.message_list:
     with st.chat_message(message['role']):
         st.write(message['content'])
-
-## AI Message 함수 정의 ###############################
-def get_ai_message(user_message):
-
-    ## 환경변수 읽어오기 ############################################
-    load_dotenv()
-    PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
-
-    ## 벡터 스토어(데이터베이스)에서 인덱스 가져오기 ################
-    ## 임베딩 모델 지정
-    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    pc = Pinecone(api_key=PINECONE_API_KEY)
-    index_name = 'laws-index'
-
-    ## 저장된 인덱스 가져오기 #######################################
-    database = PineconeVectorStore.from_existing_index(
-        index_name=index_name,
-        embedding=embedding,
-    )
-
-    ## RetrievalQA ##################################################
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
-
-    def format_docs(docs):
-        return '\n\n'.join(doc.page_content for doc in docs)
-
-    qa_chain = (
-        {
-            'context': database.as_retriever() | format_docs,
-            'question': RunnablePassthrough(),
-        }
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
-
-    ai_message = qa_chain.invoke(user_message)
-    return ai_message
 
 ## 채팅창 #############################################
 if prompt := st.chat_input('전세사기 피해와 관련된 질문을 해주세요'):
