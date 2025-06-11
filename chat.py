1d43a6a403402d8f5847c8a98270a8758a43e085
bravery33 <diddung83@gmail.com>
2025-06-11 16:21:30 +0900

    feat(chat): Implement conversational memory for chat history

--- chat.py
+++ chat.py
@@ -1,10 +1,9 @@
-# https://docs.streamlit.io/
 import streamlit as st
 from llm import get_ai_message
 
 st.set_page_config(
     page_title='전세사기피해 상담 챗봇',
-    page_icon='🤖', # 파비콘
+    page_icon='🤖',
     )
 
 st.title('🤖전세사기피해 상담 챗봇🤖')
@@ -12,21 +11,22 @@ st.title('🤖전세사기피해 상담 챗봇🤖')
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
 
-## 기존 채팅 내용 출력 
 for message in st.session_state.message_list:
     with st.chat_message(message['role']):
         st.write(message['content'])
 
-## 채팅창
 if prompt := st.chat_input('전세사기 피해와 관련된 질문을 해주세요'):
-    ## 사용자 질문
+
     with st.chat_message('user'):
         st.write(prompt)
     st.session_state.message_list.append({'role': 'user', 'content': prompt})
 
-    ## AI 답변 
     with st.chat_message('ai'):
         with st.spinner('답변을 생성하는 중입니다'):
-            ai_message = get_ai_message(prompt)
+            session_id= 'user-session'
+            ai_message = get_ai_message(
+                user_message=prompt,
+                session_id=session_id
+                )
             st.write(ai_message)
     st.session_state.message_list.append({'role': 'ai', 'content': ai_message})
--- llm.py
+++ llm.py
@@ -1,33 +1,32 @@
 import os
 
 from dotenv import load_dotenv
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain.schema import StrOutputParser
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
 
-## 환경변수 읽어오기
 load_dotenv()
+store = {}
 
-## LLM 함수 정의
 def get_llm(model='gpt-4o'):
     llm = ChatOpenAI(model=model)
     return llm
 
 
-## 데이터베이스 함수 정의
 def get_database():
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 
-    ## 임베딩 모델 지정
     embedding = OpenAIEmbeddings(model='text-embedding-3-large')
     Pinecone(api_key=PINECONE_API_KEY)
     index_name = 'laws-index'
 
-    ## 저장된 인덱스 가져오기
     database = PineconeVectorStore.from_existing_index(
         index_name=index_name,
         embedding=embedding,
@@ -35,38 +34,72 @@ def get_database():
     return database
 
 
-## RetrievalQA 함수 정의
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
 def get_retrievalQA():
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
-    ## 벡터 스토어(데이터베이스)에서 인덱스 가져오기
+
+    llm = get_llm()
     database = get_database()
 
-    # prompt = hub.pull('rlm/rag-prompt', api_key=LANGCHAIN_API_KEY)
-    prompt = hub.pull('bravery/rag-prompt', api_key=LANGCHAIN_API_KEY)
+    system_prompt = (
+    '''
+    [identity]
+    - 당신은 전세 사기 피해 법률 전문 변호사입니다.
+    - [context]를 참고하여 사용자의 질문에 답변하세요.
+    - 마지막 문단에는 답변에 해당하는 정확한 법률 조항을 기재하세요.
+    - 전세 사기 피해 관련 질문 이외에는 '해당 질문에는 답변할 수 없습니다'로 답하세요
+    [context]
+    {context}
+    '''
+    )
 
-    ## LLM 모델지정
-    llm = get_llm()
+    qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),          
+        MessagesPlaceholder('chat_history'),
+        ('human', '{input}'),               
+    ]
+    )
 
     def format_docs(docs):
         return '\n\n'.join(doc.page_content for doc in docs)
 
+
+    input_str = RunnableLambda(lambda x: x['input'])
+
     qa_chain = (
         {
-            'context': database.as_retriever() | format_docs,
-            'question': RunnablePassthrough(),
+            'context': input_str | database.as_retriever() | format_docs,
+            'chat_history': RunnableLambda(lambda x: x['chat_history']),
+            'input': input_str,
         }
-        | prompt
+        | qa_prompt
         | llm
         | StrOutputParser()
         )
-    return qa_chain
+    
+    conversational_rag_chain = RunnableWithMessageHistory(
+        qa_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        )
+    return conversational_rag_chain
 
 
-## AI Message 함수 정의 
-def get_ai_message(user_message):
-
-    ## RetrievalQA 
+def get_ai_message(user_message, session_id=None):
     qa_chain = get_retrievalQA()
-    ai_message = qa_chain.invoke(user_message)
 
+    ai_message = qa_chain.invoke(
+        {'input': user_message}, 
+        config={'configurable': {'session_id': session_id}},
+    )
+    
+    # print(f'대화 이력 >> {get_session_history(session_id)} \n\n')
+    # print('=' * 50 + '\n')
+  
     return ai_message
